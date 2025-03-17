0cf86d8d2451f88e0eabbc28c127db7d2da78c72
eljan123 <eypamintuan.work@gmail.com>
2025-03-17 13:58:05 +0800

    added opacity

--- lib/screens/todo_list_page.dart
+++ lib/screens/todo_list_page.dart
@@ -94,7 +94,7 @@ class _TodoListPageState extends State<TodoListPage> with SingleTickerProviderSt
                   Icon(
                     Icons.task_outlined,
                     size: 64,
-                    color: Colors.white,
+                    color: Colors.white.withValues(alpha: 0.6),
                   ),
                   const SizedBox(height: 16),
                   Text(
